f249a386cccb66f78dd744661678aea653ec2be4
Xuân Vương <84366191+UIT-20520353@users.noreply.github.com>
2021-12-13 10:27:33 +0700

    Cập nhật phần cài đặt

---
+++ ManageSchedule/ManageSchedule/Classes/HangSo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageSchedule
+{
+    public static class HangSo
+    {
+        //public static string strCon = @"Server=204.2.195.207,18708;Database=manageschedule;User=team4;Password=Team45678";
+        public static string strCon = System.Configuration.ConfigurationManager.ConnectionStrings["Conn"].ConnectionString;
+        public static string txtFilePath = string.Format("{0}\\Data\\setting.txt", Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\")));
+    }
+}
