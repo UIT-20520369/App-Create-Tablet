6f661793e88dab3d10e84e4f1fe9f653b134bbab
Xuân Vương <84366191+UIT-20520353@users.noreply.github.com>
2021-12-23 11:45:44 +0700

    Update UI

---
+++ ManageSchedule/ManageSchedule/ItemCongViec/FormMonthView.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace ManageSchedule
+{
+    partial class FormMonthView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlLabel = new System.Windows.Forms.Panel();
+            this.pnl4 = new System.Windows.Forms.Panel();
+            this.pnlMatrix = new System.Windows.Forms.Panel();
+            this.lblMonday = new System.Windows.Forms.Label();
+            this.lblTuesday = new System.Windows.Forms.Label();
+            this.lblWednesday = new System.Windows.Forms.Label();
+            this.lblThursday = new System.Windows.Forms.Label();
+            this.lblFriday = new System.Windows.Forms.Label();
+            this.lblSaturday = new System.Windows.Forms.Label();
+            this.lblSunday = new System.Windows.Forms.Label();
+            this.btnNextMonth = new System.Windows.Forms.Button();
+            this.btnPreviousMonth = new System.Windows.Forms.Button();
+            this.pnlLabel.SuspendLayout();
+            this.pnl4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlLabel
+            // 
+            this.pnlLabel.Controls.Add(this.btnNextMonth);
+            this.pnlLabel.Controls.Add(this.btnPreviousMonth);
+            this.pnlLabel.Controls.Add(this.lblSunday);
+            this.pnlLabel.Controls.Add(this.lblSaturday);
+            this.pnlLabel.Controls.Add(this.lblFriday);
+            this.pnlLabel.Controls.Add(this.lblThursday);
+            this.pnlLabel.Controls.Add(this.lblWednesday);
+            this.pnlLabel.Controls.Add(this.lblTuesday);
+            this.pnlLabel.Controls.Add(this.lblMonday);
+            this.pnlLabel.Location = new System.Drawing.Point(0, 0);
+            this.pnlLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlLabel.Name = "pnlLabel";
+            this.pnlLabel.Size = new System.Drawing.Size(1124, 50);
+            this.pnlLabel.TabIndex = 0;
+            // 
+            // pnl4
+            // 
+            this.pnl4.BackColor = System.Drawing.Color.White;
+            this.pnl4.Controls.Add(this.pnlMatrix);
+            this.pnl4.Location = new System.Drawing.Point(0, 51);
+            this.pnl4.Name = "pnl4";
+            this.pnl4.Size = new System.Drawing.Size(1123, 571);
+            this.pnl4.TabIndex = 1;
+            // 
+            // pnlMatrix
+            // 
+            this.pnlMatrix.Location = new System.Drawing.Point(55, 0);
+            this.pnlMatrix.Name = "pnlMatrix";
+            this.pnlMatrix.Size = new System.Drawing.Size(1013, 568);
+            this.pnlMatrix.TabIndex = 0;
+            // 
+            // lblMonday
+            // 
+            this.lblMonday.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(227)))), ((int)(((byte)(206)))));
+            this.lblMonday.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMonday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblMonday.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMonday.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblMonday.Location = new System.Drawing.Point(55, 0);
+            this.lblMonday.Margin = new System.Windows.Forms.Padding(3, 0, 1, 0);
+            this.lblMonday.Name = "lblMonday";
+            this.lblMonday.Size = new System.Drawing.Size(143, 50);
+            this.lblMonday.TabIndex = 0;
+            this.lblMonday.Text = "Thứ 2";
+            this.lblMonday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTuesday
+            // 
+            this.lblTuesday.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(227)))), ((int)(((byte)(206)))));
+            this.lblTuesday.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblTuesday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblTuesday.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTuesday.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblTuesday.Location = new System.Drawing.Point(200, 0);
+            this.lblTuesday.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblTuesday.Name = "lblTuesday";
+            this.lblTuesday.Size = new System.Drawing.Size(143, 50);
+            this.lblTuesday.TabIndex = 1;
+            this.lblTuesday.Text = "Thứ 3";
+            this.lblTuesday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblWednesday
+            // 
+            this.lblWednesday.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(227)))), ((int)(((byte)(206)))));
+            this.lblWednesday.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblWednesday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblWednesday.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWednesday.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblWednesday.Location = new System.Drawing.Point(345, 0);
+            this.lblWednesday.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblWednesday.Name = "lblWednesday";
+            this.lblWednesday.Size = new System.Drawing.Size(143, 50);
+            this.lblWednesday.TabIndex = 2;
+            this.lblWednesday.Text = "Thứ 4";
+            this.lblWednesday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblThursday
+            // 
+            this.lblThursday.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(227)))), ((int)(((byte)(206)))));
+            this.lblThursday.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblThursday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblThursday.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThursday.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblThursday.Location = new System.Drawing.Point(490, 0);
+            this.lblThursday.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblThursday.Name = "lblThursday";
+            this.lblThursday.Size = new System.Drawing.Size(143, 50);
+            this.lblThursday.TabIndex = 3;
+            this.lblThursday.Text = "Thứ 5";
+            this.lblThursday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFriday
+            // 
+            this.lblFriday.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(227)))), ((int)(((byte)(206)))));
+            this.lblFriday.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblFriday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblFriday.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFriday.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblFriday.Location = new System.Drawing.Point(635, 0);
+            this.lblFriday.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblFriday.Name = "lblFriday";
+            this.lblFriday.Size = new System.Drawing.Size(143, 50);
+            this.lblFriday.TabIndex = 4;
+            this.lblFriday.Text = "Thứ 6";
+            this.lblFriday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSaturday
+            // 
+            this.lblSaturday.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(227)))), ((int)(((byte)(206)))));
+            this.lblSaturday.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSaturday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblSaturday.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaturday.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblSaturday.Location = new System.Drawing.Point(780, 0);
+            this.lblSaturday.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblSaturday.Name = "lblSaturday";
+            this.lblSaturday.Size = new System.Drawing.Size(143, 50);
+            this.lblSaturday.TabIndex = 5;
+            this.lblSaturday.Text = "Thứ 7";
+            this.lblSaturday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSunday
+            // 
+            this.lblSunday.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(227)))), ((int)(((byte)(206)))));
+            this.lblSunday.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSunday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblSunday.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSunday.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblSunday.Location = new System.Drawing.Point(925, 0);
+            this.lblSunday.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.lblSunday.Name = "lblSunday";
+            this.lblSunday.Size = new System.Drawing.Size(143, 50);
+            this.lblSunday.TabIndex = 6;
+            this.lblSunday.Text = "Chủ nhật";
+            this.lblSunday.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNextMonth
+            // 
+            this.btnNextMonth.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(227)))), ((int)(((byte)(206)))));
+            this.btnNextMonth.FlatAppearance.BorderSize = 0;
+            this.btnNextMonth.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNextMonth.Image = global::ManageSchedule.Properties.Resources.chevron_circle_right_blue;
+            this.btnNextMonth.Location = new System.Drawing.Point(1071, 1);
+            this.btnNextMonth.Name = "btnNextMonth";
+            this.btnNextMonth.Size = new System.Drawing.Size(50, 50);
+            this.btnNextMonth.TabIndex = 8;
+            this.btnNextMonth.UseVisualStyleBackColor = false;
+            this.btnNextMonth.Click += new System.EventHandler(this.btnNextMonth_Click);
+            // 
+            // btnPreviousMonth
+            // 
+            this.btnPreviousMonth.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(134)))), ((int)(((byte)(227)))), ((int)(((byte)(206)))));
+            this.btnPreviousMonth.FlatAppearance.BorderSize = 0;
+            this.btnPreviousMonth.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPreviousMonth.Image = global::ManageSchedule.Properties.Resources.chevron_circle_left_blue;
+            this.btnPreviousMonth.Location = new System.Drawing.Point(0, 0);
+            this.btnPreviousMonth.Name = "btnPreviousMonth";
+            this.btnPreviousMonth.Size = new System.Drawing.Size(50, 50);
+            this.btnPreviousMonth.TabIndex = 7;
+            this.btnPreviousMonth.UseVisualStyleBackColor = false;
+            this.btnPreviousMonth.Click += new System.EventHandler(this.btnPreviousMonth_Click);
+            // 
+            // FormMonthView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 621);
+            this.Controls.Add(this.pnl4);
+            this.Controls.Add(this.pnlLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormMonthView";
+            this.Text = "FormMonthView";
+            this.pnlLabel.ResumeLayout(false);
+            this.pnl4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLabel;
+        private System.Windows.Forms.Label lblSunday;
+        private System.Windows.Forms.Label lblSaturday;
+        private System.Windows.Forms.Label lblFriday;
+        private System.Windows.Forms.Label lblThursday;
+        private System.Windows.Forms.Label lblWednesday;
+        private System.Windows.Forms.Label lblTuesday;
+        private System.Windows.Forms.Label lblMonday;
+        private System.Windows.Forms.Panel pnl4;
+        private System.Windows.Forms.Panel pnlMatrix;
+        private System.Windows.Forms.Button btnPreviousMonth;
+        private System.Windows.Forms.Button btnNextMonth;
+    }
+}
---
+++ ManageSchedule/ManageSchedule/ItemCongViec/FormShowSuKien.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace ManageSchedule
+{
+    partial class FormShowSuKien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flPnlTotal = new System.Windows.Forms.FlowLayoutPanel();
+            this.flPnl1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnlSukien = new System.Windows.Forms.Panel();
+            this.flPnlTotal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flPnlTotal
+            // 
+            this.flPnlTotal.Controls.Add(this.flPnl1);
+            this.flPnlTotal.Controls.Add(this.pnlSukien);
+            this.flPnlTotal.Location = new System.Drawing.Point(0, 0);
+            this.flPnlTotal.Name = "flPnlTotal";
+            this.flPnlTotal.Size = new System.Drawing.Size(421, 536);
+            this.flPnlTotal.TabIndex = 0;
+            // 
+            // flPnl1
+            // 
+            this.flPnl1.Location = new System.Drawing.Point(0, 0);
+            this.flPnl1.Margin = new System.Windows.Forms.Padding(0);
+            this.flPnl1.Name = "flPnl1";
+            this.flPnl1.Size = new System.Drawing.Size(421, 30);
+            this.flPnl1.TabIndex = 0;
+            // 
+            // pnlSukien
+            // 
+            this.pnlSukien.AutoScroll = true;
+            this.pnlSukien.AutoScrollMargin = new System.Drawing.Size(3, 3);
+            this.pnlSukien.AutoScrollMinSize = new System.Drawing.Size(3, 3);
+            this.pnlSukien.Location = new System.Drawing.Point(0, 30);
+            this.pnlSukien.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlSukien.Name = "pnlSukien";
+            this.pnlSukien.Size = new System.Drawing.Size(421, 506);
+            this.pnlSukien.TabIndex = 1;
+            // 
+            // FormShowSuKien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 536);
+            this.Controls.Add(this.flPnlTotal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormShowSuKien";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormShowSuKien";
+            this.Deactivate += new System.EventHandler(this.FormShowSuKien_Deactivate);
+            this.flPnlTotal.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flPnlTotal;
+        private System.Windows.Forms.FlowLayoutPanel flPnl1;
+        private System.Windows.Forms.Panel pnlSukien;
+    }
+}
